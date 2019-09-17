8175981e8349802290ba29d580d9d2a14c5ede77
FTWinston <ftwinston@gmail.com>
2019-09-17 23:21:03 +0100

    Initial build

---
+++ precache-manifest.7f7c508b32e1c1960b7fd22f08185501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfb03d0ed452d4ac77cc859eb0f531c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc981a9dba33a35fea40",
+    "url": "/static/css/2.90e93f6d.chunk.css"
+  },
+  {
+    "revision": "8cf677c12cd7bb9e2622",
+    "url": "/static/css/main.724e07b1.chunk.css"
+  },
+  {
+    "revision": "dc981a9dba33a35fea40",
+    "url": "/static/js/2.d330016b.chunk.js"
+  },
+  {
+    "revision": "8cf677c12cd7bb9e2622",
+    "url": "/static/js/main.7d51ca74.chunk.js"
+  },
+  {
+    "revision": "0258d8ca3d22a13ca263",
+    "url": "/static/js/runtime~main.99775e65.js"
+  }
+]);
